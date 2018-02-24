4f6142aea4ce6ee914ab3e22546aadc6b27242d3
Jacob Errington <git@mail.jerrington.me>
2018-02-24 13:08:20 -0500

    wip

---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HexGrid : MonoBehaviour {
+    public int width = 6;
+    public int height = 6;
+
+    public HexCell cellPrefab;
+
+    public Text cellLabelPrefab;
+
+    Canvas gridCanvas;
+    HexMesh hexMesh;
+
+    HexCell[] cells;
+
+    // start gets called *after* awake, so at this point, `cells` will be initialized
+    void Start () {
+        hexMesh.Triangulate(cells);
+    }
+
+    void Awake () {
+        gridCanvas = GetComponentInChildren<Canvas>();
+        hexMesh = GetComponentInChildren<HexMesh>();
+
+        Debug.Log("hello world");
+
+        cells = new HexCell[height * width];
+
+        for (int y = 0, i = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                CreateCell(x, y, i++);
+            }
+        }
+    }
+
+    void CreateCell (int x , int y, int i) {
+        // define the position for our tile
+        Vector3 position;
+        position.y = (y + x * 0.5f - x / 2) * (HexMetrics.innerRadius * 2f);
+        position.x = x * (HexMetrics.outerRadius * 1.5f);
+        position.z = 0f;
+
+        HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+
+        Text label = Instantiate<Text>(cellLabelPrefab);
+        label.rectTransform.SetParent(gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition =
+            new Vector2(position.x, position.y);
+        label.text = x.ToString() + "\n" + y.ToString();
+    }
+
+	  // Update is called once per frame
+	  void Update () {
+
+	  }
+}
---
+++ Assets/Scripts/HexMetrics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public static class HexMetrics {
+    public const float outerRadius = 10f;
+    public const float innerRadius = outerRadius * 0.866025404f;
+
+    public static Vector3[] corners;
+
+    static HexMetrics() {
+        var c = new List<Vector3>();
+        for(int i = 0; i < 6; i++) {
+            double theta = -i * Math.PI / 3;
+            c.Add(new Vector3((float)Math.Cos(theta) * outerRadius, (float)Math.Sin(theta) * outerRadius, 0f));
+        }
+        corners = c.ToArray();
+        Debug.Log("Initialized hex metrics.");
+    }
+}
