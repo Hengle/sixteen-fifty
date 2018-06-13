184283f13fff8fc5df09ae25af2b3227b079f123
Eric Mayhew <ejmayhew@gmail.com>
2018-06-13 18:28:36 -0400

    art push

--- scripts/genramp.py
+++ scripts/genramp.py
@@ -5,12 +5,12 @@ import colorsys
 import sys
 
 V = [20, 30, 45, 58, 70, 78, 85, 92, 100]
-S = [30, 40, 52, 65, 75, 63, 45, 30, 15]
-H = [350, 30, 60, 100, 140, 180, 220, 265]
+S = [30, 45, 55, 65, 75, 63, 45, 30, 15]
+H = [350, 30, 61, 100, 140, 180, 220, 265]
 
-H_ADJ = 8
+H_ADJ = 0
 
-C_ADJ = 40
+C_ADJ = 50
 
 assert(len(V) == len(S))
 
@@ -21,22 +21,22 @@ def main(dst):
     for y, h in enumerate(H):
         for i, (s, v) in enumerate(zip(S, V)):
             if (i - 4) < 0:
-                BLUE_H = 240
-                if h < 240 - 180:
-                    BLUE_H -= 360
-
-                w = (i - 4) / C_ADJ
+                if h > 60 and h < 240:
+                    BLUE_H = 240
+                else:
+                    BLUE_H = 240 - 360
+                w = abs ((i - 4) / C_ADJ)
                 hh = (BLUE_H * w + (1 - w) * h)
             else:
                 RED_H = 0
                 if h > 180:
-                    RED_H = 180
-                w = (i - 4) / C_ADJ
+                    RED_H = 360
+                w = abs ((i - 4) / C_ADJ)
                 hh = (RED_H * w + (1 - w) * h)
 
             hh += (i - 4) * H_ADJ
-                    
-            (r, g, b) = colorsys.hsv_to_rgb(hh/360, s/100, v/100)            
+
+            (r, g, b) = colorsys.hsv_to_rgb(hh/360, s/100, v/100)
 
             im.putpixel((i, y), (int (r * 255), int (g *255), int (b * 255)))
 
