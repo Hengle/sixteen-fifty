f878f0412352fe67ae13d164559c999489a0daa5
Jacob Errington <git@mail.jerrington.me>
2018-07-13 07:18:42 -0400

    SpawnPlayer: request no editor
    SpawnPlayer is not parametric, so it doesn't need an editor. We
    indicate this uses the [NoEditorNeeded] attribute.

--- Assets/Scripts/Events/EventItems/SpawnPlayer.cs
+++ Assets/Scripts/Events/EventItems/SpawnPlayer.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace SixteenFifty.EventItems {
   [Serializable]
+  [NoEditorNeeded]
   [SelectableSubtype(friendlyName = "Spawn Player")]
   public class SpawnPlayer : ImmediateScript {
     public override void Call(EventRunner runner) {
