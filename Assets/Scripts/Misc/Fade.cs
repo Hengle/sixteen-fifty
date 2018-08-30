02c1ab7cc68139aec693735d4a589998b0b5e553
Jacob Errington <git@mail.jerrington.me>
2018-08-30 10:27:26 -0400

    reorganize Misc directory and improve FadeDirection
    I implement an eliminator for FadeDirection objects, allowing
    convenient case analysis. Using this, I reimplement the existing
    FadeDirection.GetInitialValueAndMultiplier using the eliminator, and I
    implement the FadeDirection.Inverse method.
    
    This is necessary because the FadeScreen event item specifies whether
    the *screen* fades in or out, which is the *opposite* of whether the
    fade-to-black panel is fading in or out.

--- Assets/Scripts/Misc/Fade.cs
+++ Assets/Scripts/Misc/Fade.cs
@@ -3,18 +3,37 @@
 public enum FadeDirection { IN, OUT }
   
 public static class FadeDirectionExt {
-
   /**
-   * Returns the value to being the fade at and the multiplier apply to the alpha-delta.
+   * \brief
+   * Eliminates a FadeDirection into another type by supplying
+   * functions to compute a result for each case.
    */
-  public static Tuple<float, int> GetInitialValueAndMultiplier(this FadeDirection self) {
+  public static T Eliminate<T>(this FadeDirection self, Func<T> ifIn, Func<T> ifOut) {
     switch(self) {
     case FadeDirection.IN:
-      return Tuple.Create(0f, 1);
+      return ifIn();
     case FadeDirection.OUT:
-      return Tuple.Create(1f, -1);
-    default:
-      throw new Exception("FadeDirection exhaustively analyzed.");
+      return ifOut();
     }
+    throw new AnalysisExhaustedException("FadeDirection");
   }
+
+  /**
+   * \brief
+   * Gets the opposite of this direction.
+   */
+  public static FadeDirection Inverse(this FadeDirection self) =>
+    self.Eliminate(
+      () => FadeDirection.OUT,
+      () => FadeDirection.IN);
+
+  /**
+   * \brief
+   * Gets a tuple containing the initial value for fading and a
+   * multiplier for the direction to fade.
+   */
+  public static Tuple<float, int> GetInitialValueAndMultiplier(this FadeDirection self) =>
+    self.Eliminate(
+      () => Tuple.Create(0f, 1),
+      () => Tuple.Create(1f, -1));
 }
